cb216fa2eff94ebb4fe5a73bb81002fceaed2806
bluntschli1821 <danielsdessy53@yahoo.com>
2026-04-02 00:24:33 +0100

    feat: project setup, nativewind, routing, tab navigation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,17 @@
-import { Link } from "expo-router";
 import "@/globals.css";
-import React from "react";
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
+import { SafeAreaView as MYSafeAreaView } from "react-native-safe-area-context";
+import { styled } from "nativewind";
 
+const SafeAreaView = styled(MYSafeAreaView);
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-background">
+    <SafeAreaView className="flex-1 p-5 bg-background">
       <Text className="text-xl font-bold text-success">
         Welcome to Nativewind!
       </Text>
       <Link href="/subscriptions">View Subscriptions</Link>
-    </View>
+    </SafeAreaView>
   );
 }
--- app/(tabs)/subscriptions.tsx
+++ app/(tabs)/subscriptions.tsx
@@ -1,11 +1,15 @@
-import { View, Text } from "react-native";
+import { Text } from "react-native";
 import React from "react";
+import { SafeAreaView as MYSafeAreaView } from "react-native-safe-area-context";
+import { styled } from "nativewind";
+
+const SafeAreaView = styled(MYSafeAreaView);
 
 const subscriptions = () => {
   return (
-    <View>
-      <Text>subscriptions</Text>
-    </View>
+    <SafeAreaView className="flex-1 p-5 bg-background">
+      <Text className="text-xl font-bold text-success">Subscriptions</Text>
+    </SafeAreaView>
   );
 };
 
